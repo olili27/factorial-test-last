fe0ed81dc94c46171a3adf69e01a6ddbbc018228
olili27 <timmolili27@gmail.com>
2025-04-01 21:59:42 +0300

    enforce eslint rules

--- factorial.js
+++ factorial.js
@@ -4,27 +4,26 @@
  * @returns {number} The factorial of the given number
  */
 export default function factorial(number) {
-    if(typeof number !== "number") {
-        throw new TypeError("function expects only numbers");
-    }
+  if (typeof number !== "number") {
+    throw new TypeError("function expects only numbers");
+  }
 
-    if(number < 0) {
-        throw new RangeError("function expects only positive numbers")
-    }
-    
-     if (!Number.isInteger(number)) {
-       throw new TypeError("function expects only positive integers");
-     }
+  if (number < 0) {
+    throw new RangeError("function expects only positive numbers");
+  }
 
+  if (!Number.isInteger(number)) {
+    throw new TypeError("function expects only positive integers");
+  }
 
   let result = 1;
 
+  // eslint-disable-next-line no-plusplus, no-param-reassign
   for (number; number > 0; number--) {
-    result = result * number;
+    result *= number;
   }
 
   return result;
 }
 
 // console.log(factorial(7.0));
-
--- test/factorial.js
+++ test/factorial.js
@@ -1,73 +1,89 @@
-import {assert} from "chai";
+/* eslint-disable no-undef */
+/* eslint-disable import/extensions */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+import { assert } from "chai";
 import factorial from "../factorial.js";
 
+// timothy olili
+// bridget nagasha
+
 describe("Factorial Function for Positive Integers", function () {
-    describe("Zero and Positive Integers", function () {
-        it("factorial(0) should return 1", function () {
-            assert.strictEqual(factorial(0), 1);
-        });
-
-        it("factorial(1) should return 1", function () {
-          assert.strictEqual(factorial(1), 1);
-        });
-
-         it("factorial(4) should return 24", function () {
-           assert.strictEqual(factorial(4), 24);
-         });
-
-          it("factorial(13) should return 6227020800", function () {
-            assert.strictEqual(factorial(13), 6227020800);
-          });
+  describe("Zero and Positive Integers", function () {
+    it("factorial(0) should return 1", function () {
+      assert.strictEqual(factorial(0), 1);
     });
 
-    describe("Negative Integers", function() {
-        it("factorial(-1) should throw an error `function expects only positive numbers`", function () {
-          assert.throws(function () {
-            factorial(-1);
-          }),
-            RangeError,
-            "function expects only positive numbers";
-        });
+    it("factorial(1) should return 1", function () {
+      assert.strictEqual(factorial(1), 1);
+    });
 
-        it("factorial(-21) should throw an error `function expects only positive numbers`", function () {
-          assert.throws(function () {
-            factorial(-21);
-          }),
-            RangeError,
-            "function expects only positive numbers";
-        });
-    })
+    it("factorial(4) should return 24", function () {
+      assert.strictEqual(factorial(4), 24);
+    });
 
-    describe("Floats", function () {
-      it("factorial(1.1) should throw an error `function expects only positive integers`", function () {
-        assert.throws(function () {
+    it("factorial(13) should return 6227020800", function () {
+      assert.strictEqual(factorial(13), 6227020800);
+    });
+  });
+
+  describe("Negative Integers", function () {
+    it("factorial(-1) should throw an error `function expects only positive numbers`", function () {
+      assert.throws(
+        function () {
+          factorial(-1);
+        },
+        RangeError,
+        "function expects only positive numbers",
+      );
+    });
+
+    it("factorial(-21) should throw an error `function expects only positive numbers`", function () {
+      assert.throws(
+        function () {
+          factorial(-21);
+        },
+        RangeError,
+        "function expects only positive numbers",
+      );
+    });
+  });
+
+  describe("Floats", function () {
+    it("factorial(1.1) should throw an error `function expects only positive integers`", function () {
+      assert.throws(
+        function () {
           factorial(1.1);
-        }),
-          TypeError,
-          "function expects only positive integers";
-      });
-
-      it("factorial(7.0) return 5040", function () {
-        assert.strictEqual(factorial(7.0), 5040)
-      });
+        },
+        TypeError,
+        "function expects only positive integers",
+      );
     });
 
-    describe("Non-Numeric", function () {
-      it("factorial('tim') should throw an error `function expects only numbers`", function () {
-        assert.throws(function () {
+    it("factorial(7.0) return 5040", function () {
+      assert.strictEqual(factorial(7.0), 5040);
+    });
+  });
+
+  describe("Non-Numeric", function () {
+    it("factorial('tim') should throw an error `function expects only numbers`", function () {
+      assert.throws(
+        function () {
           factorial("tim");
-        }),
-          Error,
-          "function expects only numbers";
-      });
-
-      it("factorial('') should throw an error `function expects only numbers`", function () {
-        assert.throws(function () {
-          factorial("");
-        }),
-          Error,
-          "function expects only numbers";
-      });
-
+        },
+        Error,
+        "function expects only numbers",
+      );
     });
+
+    it("factorial('') should throw an error `function expects only numbers`", function () {
+      assert.throws(
+        function () {
+          factorial("");
+        },
+        Error,
+        "function expects only numbers",
+      );
+    });
+  });
 });
